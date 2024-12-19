2d3029cbee6c6de0569209326cb68168f9813fa3
riswantohalim12 <riswanajalah01@gmail.com>
2024-12-20 04:14:19 +0700

    Update script.js

--- project/laporan penjualan/script.js
+++ project/laporan penjualan/script.js
@@ -14,14 +14,14 @@ const salesChart = new Chart(ctx, {
                 data: [4017, 4136, 7091, 7555, 5481, 5467, 7130, 6133, 3995, 2897, 5235, 5472],
                 backgroundColor: 'rgba(255, 99, 132, 0.6)',
                 borderColor: 'rgba(255, 99, 132, 1)',
-                borderWidth: 1
+                borderWidth: 4
             },
             {
                 label: '2023',
                 data: [6135, 7416, 5841, 5841, 6547, 3467, 6343, 9340, 2977, 8879, 5435, 889],
                 backgroundColor: 'rgba(54, 162, 235, 0.6)',
                 borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1
+                borderWidth: 4
             }
         ]
     },
